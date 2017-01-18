85816b3c5a6181ebd9535423464fadbcdb07ef4b
MichaelNg97 <michaelngteckhua@gmail.com>
2017-01-18 00:06:22 -0500

    Hello World (GitHub Integration)

---
+++ src/hell0w0rld/Hell0W0rld.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hell0w0rld;
+
+/**
+ *
+ * @author Michael
+ */
+public class Hell0W0rld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.printf("Hello World, my GitHub account name is TeckHuaNg %n");
+    }
+    
+}
